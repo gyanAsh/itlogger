89495cc10b2acc0a04cc88aaa5cb7b911e8a6b25
gyanAsh <gyanash36@protonmail.com>
2022-06-23 00:34:17 +0530

    Added conditions while submitting the form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Fragment , useEffect } from 'react';
 import SearchBar from './Components/Layout/SearchBar';
 import Logs from './Components/Logs/Logs';
+import AddBtn from './Components/Layout/AddBtn';
+import AddLogModal from './Components/Logs/AddLogModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -15,6 +17,8 @@ const App = () => {
     <Fragment className="App">
       <SearchBar />
       <div className="container">
+        <AddBtn />
+        <AddLogModal />
         <Logs/>
       </div>
     </Fragment>
