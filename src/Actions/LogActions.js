003d31d118ee3e1b9ef3f82877c3ec071d501366
gyanAsh <gyanash36@protonmail.com>
2022-06-25 12:28:44 +0530

    Delete Logs

--- src/Actions/LogActions.js
+++ src/Actions/LogActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR,ADD_LOG } from './Types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR,ADD_LOG ,DELETE_LOG } from './Types';
 
 // export const getLogs = () => {
 //     return async dispatch => {
@@ -63,6 +63,27 @@ export const addLog = (log) => async dispatch => {
     }
 };
 
+// Delete Log from server
+export const deleteLog = (id) => async dispatch => {
+
+    try {
+        setLoading();
+
+        await fetch(`/logs/${id}`, {
+            method: 'DELETE',
+
+        });
+        dispatch({
+            type: DELETE_LOG,
+            payload: id
+        })
+    } catch (err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.data
+        })
+    }
+};
 
 // Set loading to true;
 export const setLoading = () => {
--- src/Reducers/LogReducer.js
+++ src/Reducers/LogReducer.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR,ADD_LOG } from '../Actions/Types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR,ADD_LOG ,DELETE_LOG } from '../Actions/Types';
 
 const initalState = {
     logs: null,
@@ -20,6 +20,12 @@ export default (state = initalState, action) => {
                 ...state,
                 logs: [...state.logs, action.payload],
                 loading:false
+            }
+        case DELETE_LOG:
+            return {
+                ...state,
+                logs: state.logs.filter(log => log.id !== action.payload),
+                loading: false
             }
         case SET_LOADING:
             return {
